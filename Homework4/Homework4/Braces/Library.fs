e655640c2a6c2277fe06e0977dbfe953d66ec9b8
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2024-04-05 23:58:09 +0300

    feat: add solution

---
+++ Homework4/Homework4/Braces/Library.fs
@@ -0,0 +1,19 @@
+﻿open System.Collections.Generic
+
+let checkBrackets (s: string) =
+    let stack = Stack<char>()
+    let bracketPairs = dict [ '(', ')'; '[', ']'; '{', '}' ]
+    let openingBrackets = Set.ofSeq bracketPairs.Keys
+    let closingBrackets = Set.ofSeq bracketPairs.Values
+    
+    s |> Seq.fold (fun acc c ->
+        match acc, openingBrackets.Contains(c), closingBrackets.Contains(c) with
+        | false, _, _ -> false
+        | _, true, _ -> 
+            stack.Push(c)
+            acc
+        | _, _, true when stack.Count > 0 && bracketPairs.[stack.Peek()] = c -> 
+            stack.Pop() |> ignore
+            acc
+        | _, _, true -> false
+        | _ -> acc) true && stack.Count = 0
