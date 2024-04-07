e2f7357bc9c6fb84e5c76957d891bf803d15a6f0
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2024-04-07 16:22:03 +0300

    feat: add library for point free

---
+++ Homework4/Homework4/PointFree/Library.fs
@@ -0,0 +1,10 @@
+﻿module PointFree
+
+let pointFreeFunc = List.map << (*) 
+
+//Шаги 
+//      List.map (fun y -> y * x) l ->
+//      List.map (fun y -> y * x) ->
+//      List.map ((*) x) ->
+//      x |> (List.map << (*)) ->
+//      List.map << (*)
