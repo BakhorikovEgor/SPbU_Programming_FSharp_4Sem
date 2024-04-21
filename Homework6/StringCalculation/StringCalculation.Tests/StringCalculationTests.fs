3b761f9ca6c613b41585ba3e668c9953be6937b1
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2024-04-21 17:06:31 +0300

    feat: done string calculation task with tests

---
+++ Homework6/StringCalculation/StringCalculation.Tests/StringCalculationTests.fs
@@ -0,0 +1,26 @@
+module Tests
+
+open NUnit.Framework
+open FsUnit
+open StringCalculation
+
+
+[<Test>]
+let ``Adding valid numbers should return Success``() =
+    let result = calculate {
+        let! x = "3"
+        let! y = "4"
+        return x + y
+    }
+    result |> should equal (Success 7)
+
+[<Test>]
+let ``Adding invalid input should return Error``() =
+    let result = calculate {
+        let! x = "3"
+        let! y = "abc"
+        return x + y
+    }
+    match result with
+    | Error msg -> msg |> should startWith "Invalid number"
+    | _ -> failwith "Expected error"
