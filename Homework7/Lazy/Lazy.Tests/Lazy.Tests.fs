2b2a3dcd577a20ea61580f20964b8d0f5fb549f5
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2024-05-30 22:46:53 +0300

    feat: add library + tests

---
+++ Homework7/Lazy/Lazy.Tests/Lazy.Tests.fs
@@ -0,0 +1,75 @@
+﻿module LazyTests
+
+open System
+open System.Threading
+open NUnit.Framework
+open FsUnit
+open Lasy
+
+
+[<TestFixture>]
+type LazyTests() =
+
+    let supplier () =
+        printfn "Computing value"
+        42
+
+    [<Test>]
+    member _.``SingleThreadLazy should compute value only once``() =
+        let l = SingleThreadLazy(supplier) :> ILazy<_>
+        l.Get() |> should equal 42
+        l.Get() |> should equal 42
+
+    [<Test>]
+    member _.``MultiThreadLazy should compute value only once``() =
+        let l = MultiThreadLazy(supplier) :> ILazy<_>
+        let results = Array.init 10 (fun _ -> l.Get())
+        results |> Array.forall ((=) 42) |> should equal true
+
+    [<Test>]
+    member _.``LockFreeLazy should compute value only once``() =
+        let l = LockFreeLazy(supplier) :> ILazy<_>
+        let results = Array.init 10 (fun _ -> l.Get())
+        results |> Array.forall ((=) 42) |> should equal true
+
+    [<Test>]
+    member _.``Concurrency test for LockFreeLazy``() =
+        let l = LockFreeLazy(supplier) :> ILazy<_>
+        let results = Array.zeroCreate 10
+        let threads =
+            [| for i in 0..9 ->
+                Thread(ThreadStart(fun () -> results.[i] <- l.Get())) |]
+        threads |> Array.iter (fun t -> t.Start())
+        threads |> Array.iter (fun t -> t.Join())
+        results |> Array.forall ((=) 42) |> should equal true
+
+    [<Test>]
+    member _.``SingleThreadLazy should work correctly with different values``() =
+        let supplier1 () = 1
+        let supplier2 () = "test"
+        let lazyInt = SingleThreadLazy(supplier1) :> ILazy<_>
+        let lazyString = SingleThreadLazy(supplier2) :> ILazy<_>
+        lazyInt.Get() |> should equal 1
+        lazyString.Get() |> should equal "test"
+
+    [<Test>]
+    member _.``MultiThreadLazy should work correctly with different values``() =
+        let supplier1 () = 1
+        let supplier2 () = "test"
+        let lazyInt = MultiThreadLazy(supplier1) :> ILazy<_>
+        let lazyString = MultiThreadLazy(supplier2) :> ILazy<_>
+        let resultsInt = Array.init 10 (fun _ -> lazyInt.Get())
+        let resultsString = Array.init 10 (fun _ -> lazyString.Get())
+        resultsInt |> Array.forall ((=) 1) |> should equal true
+        resultsString |> Array.forall ((=) "test") |> should equal true
+
+    [<Test>]
+    member _.``LockFreeLazy should work correctly with different values``() =
+        let supplier1 () = 1
+        let supplier2 () = "test"
+        let lazyInt = LockFreeLazy(supplier1) :> ILazy<_>
+        let lazyString = LockFreeLazy(supplier2) :> ILazy<_>
+        let resultsInt = Array.init 10 (fun _ -> lazyInt.Get())
+        let resultsString = Array.init 10 (fun _ -> lazyString.Get())
+        resultsInt |> Array.forall ((=) 1) |> should equal true
+        resultsString |> Array.forall ((=) "test") |> should equal true
---
+++ Homework7/Lazy/Lazy/Lazy.fs
@@ -0,0 +1,46 @@
+﻿namespace Lasy
+
+open System.Threading
+
+type ILazy<'a> =
+    abstract member Get: unit -> 'a
+
+type SingleThreadLazy<'a>(supplier: unit -> 'a) =
+    let mutable value = None
+    interface ILazy<'a> with
+        member this.Get() =
+            match value with
+            | Some v -> v
+            | None ->
+                let result = supplier()
+                value <- Some result
+                result
+
+
+type MultiThreadLazy<'a>(supplier: unit -> 'a) =
+    let mutable value = None
+    let lockObj = obj()
+    interface ILazy<'a> with
+        member this.Get() =
+            lock lockObj (fun () ->
+                match value with
+                | Some v -> v
+                | None ->
+                    let result = supplier()
+                    value <- Some result
+                    result)
+
+
+type LockFreeLazy<'a>(supplier: unit -> 'a) =
+    let mutable value = None : Option<'a>
+    let mutable isInitialized = 0
+    interface ILazy<'a> with
+        member this.Get() =
+            if Interlocked.CompareExchange(&isInitialized, 1, 0) = 0 then
+                let result = supplier()
+                value <- Some result
+                result
+            else
+                while value.IsNone do
+                    Thread.SpinWait(1)
+                value.Value
